1531d7df8bf69c6356d6b31b44b9214f4de0afdb
annaship <tetya-loshad@yandex.ru>
2011-06-21 16:17:23 -0400

    function to add link, works

--- docs/my_functions.php
+++ docs/my_functions.php
@@ -1,16 +1,9 @@
 <?php 
 include("my_arrays.php");
 
-function create_names($entry) {
-	$words = preg_split("/[\s]+/", $entry);
-	return $words;
-}
-
-function SearchArray($array, $search) {
+function search_array($array, $search) {
   $result = "";
-  // print_r($array);
   foreach($array as $item) {
-		// echo "HERE ".$search."<p/>";
     if (preg_match("|{$search}|i", $item)) {
         $result = $item;
 				// echo "result =  ".$result."<p/>";
@@ -19,62 +12,27 @@ function SearchArray($array, $search) {
   return $result;
 }
 
-
-// function split_names() {
-// 	global $name_links;
-// 	$splitted_names = array();
-// 		foreach (array_keys($name_links) as name_link) {
-// 			
-// 		}
-// 		
-// 		// $splitted_names += preg_split("/[\s]+/", $full_name);
-// 	// print_r($splitted_names);
-// 	
-// 	return $splitted_names;
-// }
-
-
-
 function add_link($entry) {
 	global $name_links;
-	$words = create_names($entry);
+	$words = preg_split("/[\s]+/", $entry);
 	$word  = "";
 	$link  = "";
 	$new_entry = "";
-	// print_r($words);
-	// Array ( [0] => [1] => ) Array ( [0] => 08/03/11 )
-	// Array ( [0] => Silva [1] => Prüße ) 
 
-	// print_r($col);
-	// SimpleXMLElement Object ( [0] => ) SimpleXMLElement Object ( [0] => 08/03/11 ) SimpleXMLElement Object ( [0] => Silva Prüße )
-	
 	foreach ($words as $word) {
 		// echo "URA ".$word."<p/>";
-		$full_name = SearchArray(array_keys($name_links), $word);
+		$full_name = search_array(array_keys($name_links), $word);
 		if ($full_name) {
 			// echo "full_name _".$full_name."_<p/>";
 			$link = $name_links[$full_name];	
-			$new_entry .= $link." ";
+			$new_entry .= "<a href=\"".$link."\">".$word."</a>";
 		}
 		else {
 			$new_entry .= $word." ";	
 		}
 	}
-	echo "new_entry ".$new_entry."<p/>";
-	// URA http://jbpc.mbl.edu/labs-msogin.html
-
-	// foreach ($words as $word) {
-	// 	// echo "URA ".$word."<p/>";
-	// 	
-	// }
-	// // echo "END"; 
-	// URA Intro
-	// URA Sogin
-	// 
-	// END
+	return $new_entry;
 }
-
-// $keywords = preg_split("/[\s,]+/", "hypertext language, programming");
 
 
 ?>
