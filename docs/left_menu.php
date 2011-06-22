2fbcf888aeb688de51e6421d70ba2f19bcc6b294
annaship <tetya-loshad@yandex.ru>
2011-06-22 17:11:57 -0400

    use function to create left menu, intermediate 2, works. the same for tools

--- docs/left_menu.php
+++ docs/left_menu.php
@@ -13,7 +13,6 @@
 				<ul>
 					<!-- <li><a class="side-link" target="_blank" href="http://jbpc.mbl.edu/labs-msogin.html">Mitchell Sogin</a></li> -->
 					<?php
-						// include 
 						foreach (array_keys($name_links) as $full_name) {
 							echo "<li>" . build_link($full_name, $name_links[$full_name], "side-link") . "</li>";
 						}
@@ -24,21 +23,11 @@
 		</div>
 		<div class="sub-list">
 			<ul>
-				<li><a class="side-link" target="_blank" href="http://code.google.com/p/ampliconnoise/">Ampliconnoise</a></li>
-				<li><a class="side-link" target="_blank" href="http://www.arb-home.de/">ARB</a></li>
-				<!-- <li><a class="side-link" target="_blank" href="">CatchAll</a></li> -->
-				<li>CatchAll</li>
-				<li><a class="side-link" target="_blank" href="http://plaza.ufl.edu/sunyijun/ESPRIT.htm">ESPRIT</a></li>
-				<li><a class="side-link" target="_blank" href="http://www.mothur.org/wiki/Main_Page">Mothur</a></li>
-				<li><a class="side-link" target="_blank" href="http://metagenomics.anl.gov/">MGRAST</a></li>
-				<!-- <li><a class="side-link" target="_blank" href="">PhyloPythia</a></li> -->
-				<li>PhyloPythia</li>
-				<li><a class="side-link" target="_blank" href="http://qiime.sourceforge.net/">QIIME</a></li>
-				<li><a class="side-link" target="_blank" href="http://rdp.cme.msu.edu/">RDP</a></li>
-				<li><a class="side-link" target="_blank" href="http://www.arb-silva.de/">Silva</a></li>
-				<li><a class="side-link" target="_blank" href="http://www.drive5.com/usearch/">UClust</a></li>
-				<li><a class="side-link" target="_blank" href="http://bmf2.colorado.edu/unifrac/">Unifrac</a></li>
-				<li><a class="side-link" target="_blank" href="http://vamps.mbl.edu/">VAMPS</a></li>
+				<?php
+					foreach (array_keys($tool_links) as $tool_name) {
+						echo "<li>" . build_link($tool_name, $tool_links[$tool_name], "side-link") . "</li>";
+					}
+				?>					
 			</ul>
 		</div>
 		<div class="side-title">Computer Facilities
--- docs/my_arrays.php
+++ docs/my_arrays.php
@@ -19,6 +19,22 @@ $name_links = array(
 	"Peter Turnbaugh" => "http://turnbaugh.openwetware.org/"
 );
 
+$tool_links = array(
+"Ampliconnoise"			=> "http://code.google.com/p/ampliconnoise/",
+"ARB"								=> "http://www.arb-home.de/",
+"CatchAll"					=> "",
+"ESPRIT"						=> "http://plaza.ufl.edu/sunyijun/ESPRIT.htm",
+"Mothur"						=> "http://www.mothur.org/wiki/Main_Page",
+"MGRAST"						=> "http://metagenomics.anl.gov/",
+"PhyloPythia"				=> "",
+"QIIME"							=> "http://qiime.sourceforge.net/",
+"RDP"								=> "http://rdp.cme.msu.edu/",
+"Silva"							=> "http://www.arb-silva.de/",
+"UClust"						=> "http://www.drive5.com/usearch/",
+"Unifrac"						=> "http://bmf2.colorado.edu/unifrac/",
+"VAMPS"							=> "http://vamps.mbl.edu/",
+);
+
 $description_words = array(
 '2-dimensional', '3-dimensional', 'administered', 'adverse', 'afflicted', 'air', 'aircraft', 'airetite', 'airplane', 'aisles', 'all', 'allowed', 'alright', 'amount', 'animals', 'area', 'associated', 'atmospheric', 'attached', 'available', 'average', 'bacteria', 'bedroom', 'benzene', 'better', 'between', 'building', 'carpet', 'category', 'ceiling', 'cfm', 'chemical', 'cigarettes', 'circulated', 'cleaned', 'cleaning', 'closed', 'coliform', 'collect', 'collected', 'collection', 'commercial', 'compounds', 'concentration', 'construction', 'cooling', 'countertop', 'covered', 'cracks', 'cultivation-based', 'cycle', 'daily', 'day', 'defined', 'defines', 'depending', 'description', 'difference', 'dissolved', 'distance', 'door', 'duration', 'during', 'dust', 'duty', 'each', 'effects', 'efficiency', 'either', 'electric', 'endotoxins', 'energy', 'engine', 'environement', 'environment', 'environmental', 'equipment', 'ergosetrol', 'ether', 'ethylbenzene', 'euclidean', 'exact', 'example', 'experiment', 'exposed', 'expressed', 'extent', 'fan', 'fast', 'fecal', 'fiber', 'field', 'filter', 'flights', 'floor', 'flooring', 'flowing', 'foot', 'formaldehyde', 'fresh', 'from', 'fuel', 'fungal', 'garage', 'gas', 'gasoline', 'gathered', 'general', 'generally', 'grades', 'hard', 'health', 'height', 'heterotrophs', 'highway', 'hour', 'house', 'htm', 'hvac', 'identifier', 'include', 'indoor', 'informative', 'initial', 'insecticide', 'inside', 'installation', 'installment', 'instance', 'intensity', 'interest', 'interval', 'into', 'journeys', 'lamp', 'leaking', 'level', 'light', 'list', 'lit', 'load', 'located', 'location', 'lower', 'lpg', 'luminous', 'many', 'mass', 'material', 'matter', 'maximum', 'means', 'measure', 'measured', 'mechanical', 'metal', 'meters', 'methyl', 'mica', 'microbes', 'mileage', 'million', 'mixed', 'model', 'months', 'more', 'motion', 'moving', 'much', 'multiple', 'nicotine', 'nm', 'no', 'no2', 'no3', 'normally', 'not', 'noticed', 'number', 'o2', 'object', 'occupants', 'occupied', 'occupies', 'occupy', 'occurs', 'often', 'open', 'org', 'organic', 'outdoor', 'outside', 'ozone', 'particle', 'particular', 'particulate', 'parts', 'pass', 'passage', 'passed', 'passenger', 'people', 'percent', 'period', 'person', 'personelle', 'planotx', 'plants', 'plastic', 'plumbing', 'pool', 'power', 'precipitation', 'presence', 'present', 'pressure', 'princeton', 'produced', 'professionals', 'provides', 'rate', 'ratio', 'receptacle', 'released', 'replaced', 'replaces', 'replica', 'room', 'route', 'sample', 'sampling', 'schoolchildren', 'seasonal', 'seat', 'seer', 'setonresourcecenter', 'sf6', 'shortest', 'since', 'single', 'site', 'size', 'smokers', 'solid', 'sound', 'source', 'space', 'specific', 'specify', 'spends', 'spent', 'square', 'stage', 'status', 'stop', 'strategies', 'substance', 'such', 'summer-grade', 'surface', 'system', 'temperature', 'terms', 'tert-butyl', 'test', 'threshold', 'time', 'to', 'total', 'toulene', 'traces', 'transfer', 'traversed', 'treatment', 'true', 'truncated', 'type', 'typical', 'typically', 'ultrafine', 'units', 'use', 'usually', 'vapor', 'vehicle', 'volatile', 'walls', 'water', 'watts', 'whether', 'wikipedia', 'window', 'winter-grade', 'wood', 'workflow', 'www', 'xylene', 'year');
 ?>
