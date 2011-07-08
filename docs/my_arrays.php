82168a38c5cb0a104dbcfb1fcbc9be3ac32f0bbd
annaship <tetya-loshad@yandex.ru>
2011-07-08 13:21:44 -0400

    andersen

--- docs/my_arrays.php
+++ docs/my_arrays.php
@@ -2,7 +2,7 @@
 $name_links = array(
 	"Mitchell Sogin" 	=> "http://jbpc.mbl.edu/labs-msogin.html",
 	"David Mark Welch"=> "http://jbpc.mbl.edu/labs-dmarkwelch.html",
-	"Gary Anderson" 	=> "http://esd.lbl.gov/ESD_staff/andersen/index.html",
+	"Gary Andersen" 	=> "http://esd.lbl.gov/ESD_staff/andersen/index.html",
 	"John Bunge" 			=> "http://www.stat.cornell.edu/~bunge/",
 	"James Cole" 			=> "http://rdp.cme.msu.edu/index.jsp",
 	"Robert Edgar" 		=> "http://www.drive5.com/more.html",
