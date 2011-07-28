9b4126f620cf3e1e65dddf17d2dcc970b10fce5b
annaship <tetya-loshad@yandex.ru>
2011-07-28 15:32:48 -0400

    work with word only if len > 1

--- docs/my_functions.php
+++ docs/my_functions.php
@@ -21,9 +21,9 @@ function create_link($entry, $my_class="") {
 	$words = preg_split("/[\s]+/", $entry);
 	$res_link = "";
 	foreach ($words as $word) {
-		$string = preg_replace('/[:,-]/','',$word);
 		if (strlen($word) > 1) {
-			$full_name = search_array(array_keys($name_links), "\b".$string."\b");	
+			$search_string = preg_replace('/[:,-]/','',$word);
+			$full_name = search_array(array_keys($name_links), "\b".$search_string."\b");	
 			if ($full_name) {
 				$link = $name_links[$full_name];	
 				$res_link .= build_link($word, $link, $my_class="");
